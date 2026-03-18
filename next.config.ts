99106d08a879ea5e948af7f27388b420f0458552
Luis-Gozzo <lgozzo1206@gmail.com>
2026-03-18 02:13:37 -0400

    Fix: Configuración de imágenes remotas en next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org", // Dominio de las imágenes de la API de películas
+        pathname: "/t/p/**", // Ruta de las carpetas de pósters
+      },
+    ],
+  },
+  /* Si tienes otras opciones de configuración, van aquí abajo */
 };
 
 export default nextConfig;
